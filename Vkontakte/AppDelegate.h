f42a6e7f0e2e7597c730a14b8b1b19f05aa1333a
GAGaliev <galievaynur@Mac-mini-Galiev.local>
2014-10-10 16:00:37 +0400

    Initial Commit

---
+++ Vkontakte/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Vkontakte
+//
+//  Created by Galiev Aynur on 10.10.14.
+//  Copyright (c) 2014 FT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Vkontakte/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Vkontakte
+//
+//  Created by Galiev Aynur on 10.10.14.
+//  Copyright (c) 2014 FT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
